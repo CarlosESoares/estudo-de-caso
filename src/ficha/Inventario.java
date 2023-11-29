2f02e86ef043a910f8bf2920d7e8fa2fb9388c83
dimi666tri <camila.s11@aluno.ifsc.edu.br>
2023-11-29 17:15:58 -0300

    modificação do aOro
    aOro= Armas ou Objetos

--- src/ficha/Inventario.java
+++ src/ficha/Inventario.java
@@ -3,6 +3,22 @@ package ficha;
 public class Inventario {
 	String objeto;
 	String descObjeto;
+	
+	public String getObjeto() {
+		return objeto;
+	}
+
+	public void setObjeto(String objeto) {
+		this.objeto = objeto;
+	}
+
+	public String getDescObjeto() {
+		return descObjeto;
+	}
+
+	public void setDescObjeto(String descObjeto) {
+		this.descObjeto = descObjeto;
+	}
 
 	public Inventario(String objeto, String descObjeto) {
 		super();
